ee5a83cffd802665672bfd932c508d14afe42c8c
AnyonyWilliam <54343267+AntonyWilliam@users.noreply.github.com>
2022-08-25 12:33:06 -0300

    Master Finar

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-var number = null;
+let number = null;
 
 function hidde_show(id, id2) {
     if(number == null){
-        var warning = document.getElementById("alert");
+        let warning = document.getElementById("alert");
         warning.classList.add("shake");
         warning.style.opacity = "1";
         return
     }
 
     else{
-        var section = document.getElementById(id);
+        let section = document.getElementById(id);
         section.classList.add("hidden");
 
 
-        var section2 = document.getElementById(id2);
+        let section2 = document.getElementById(id2);
         section2.classList.remove("hidden");
 
-        var warning = document.getElementById("alert");
+        let warning = document.getElementById("alert");
         warning.classList.remove("shake");
         warning.style.opacity = "0";
     }
 }
 
 function alterValue(value){
-    var number2 = document.getElementById("number");
+    let number2 = document.getElementById("number");
     number2.innerHTML = value;
     number = value;
 }
