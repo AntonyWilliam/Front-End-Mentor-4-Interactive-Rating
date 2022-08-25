455af0eeed404657688d74766dc703de291567a9
AnyonyWilliam <54343267+AntonyWilliam@users.noreply.github.com>
2022-08-25 11:26:50 -0300

    Final

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-function hidde(id) {
-    var section = document.getElementById(id);
-    section.classList.add("hidden");
-}
+var number = null;
 
-function show(id){
-    var section = document.getElementById(id);
-    section.classList.remove("hidden");
+function hidde_show(id, id2) {
+    if(number == null){
+        var warning = document.getElementById("alert")
+        warning.style.opacity = "1"
+        return
+    }
+
+    else{
+        var section = document.getElementById(id);
+        section.classList.add("hidden");
+
+
+        var section2 = document.getElementById(id2);
+        section2.classList.remove("hidden");
+
+        var warning = document.getElementById("alert")
+        warning.style.opacity = "0"
+    }
 }
 
 function alterValue(value){
-    var number = document.getElementById("number");
-    number.innerHTML = value;
+    var number2 = document.getElementById("number");
+    number2.innerHTML = value;
+    number = value;
 }
